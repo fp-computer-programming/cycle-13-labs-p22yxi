d3f8d2bfefe74f23100dac3b30a4f0b554203556
p22yxi <p22yxi@fairfieldprep.org>
2022-03-09 10:20:00 -0500

    lab-2-1(working)

--- lab_2-1.py
+++ lab_2-1.py
@@ -4,11 +4,14 @@
 class car:
     def __build_car__(wheels, axels, doors, color):
         wls = wheels
-        wheels = int(input("How many wheels does this car have? "))
         als = axels
-        axels = int(input("How many axels does this car have? "))
         dos  = doors
-        doors = int(input("How many doors does this car have? "))
         cor = color
+
+
+
+        doors = int(input("How many doors does this car have? "))
+        axels = int(input("How many axels does this car have? "))
         color = input("What is color of this car? ")
+        wheels = int(input("How many wheels does this car have? "))
     print('This car has', wls, 'wheels,', axels, 'axels,', doors, 'doors and', color, 'color')
